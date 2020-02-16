3cf7643257941834d70808e0effdfcc4da00cd0d
Nickaël Rakotonindrina <nickael.rakotonindrina@outlook.com>
2020-02-16 01:37:34 +0100

    (master) testing oh my zsh auto install#wip

--- tools/install.sh
+++ tools/install.sh
@@ -94,20 +94,6 @@ main() {
   fi
   cp "$ZSH_NICKS_THEME"/templates/aliases.sh ~/.aliases.sh
 
-  # If this user's login shell is not already "zsh", attempt to switch.
-  TEST_CURRENT_SHELL=$(expr "$SHELL" : '.*/\(.*\)')
-  if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
-    # If this platform provides a "chsh" command (not Cygwin), do it, man!
-    if hash chsh >/dev/null 2>&1; then
-      printf "${BLUE}Time to change your default shell to zsh!${NORMAL}\n"
-      chsh -s $(grep /zsh$ /etc/shells | tail -1)
-    # Else, suggest the user do so manually.
-    else
-      printf "I can't change your shell automatically because this system does not have chsh.\n"
-      printf "${BLUE}Please manually change your default shell to zsh!${NORMAL}\n"
-    fi
-  fi
-
   env git config --global alias.br branch && git config --global alias.co checkout && git config --global alias.ci commit && git config --global alias.st status
 
   printf "${GREEN}"
