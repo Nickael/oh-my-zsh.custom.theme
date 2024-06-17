dfaaf444f128492bd6fdb0eeac15ee9566a93e8e
Nickaël Rakotonindrina <nrakotonindrina@icebergdatalab.com>
2024-06-17 13:16:15 +0200

    [master] updated aliases

--- templates/aliases.sh
+++ templates/aliases.sh
@@ -1,20 +1,6 @@
-alias l="ls"
-alias ll="ls -l"
-alias la="ls -la"
-alias a="ls -a"
+#!/bin/sh
 
-alias cp="cp -v"
-alias mv="mv -v"
-alias mkdir="mkdir -v"
-alias rm="rm -v"
-alias ln="ln -v"
-
-alias z="vi ~/.zshrc"
-alias b="vi ~/.bashrc"
-alias h="vi ~/.zsh_history"
-alias sz="source ~/.zshrc"
-alias va="vi ~/.aliases.sh"
-
+# region conditional aliases
 if hash vim 2> /dev/null; then
     alias vi="vim"
 fi
@@ -26,17 +12,97 @@ if hash docker-compose 2> /dev/null; then
     alias dk="docker-compose"
 fi
 
+if hash code-insiders 2> /dev/null; then
+    alias code="code-insiders"
+fi
+
 if hash apt 2>/dev/null; then
     alias i="sudo apt update && sudo apt install -y"
     alias r="sudo apt autoremove --purge"
     alias s="sudo apt-cache search"
     alias u="sudo apt update && sudo apt upgrade"
-else
+elif hash yum 2>/dev/null; then
     alias i="sudo yum update && sudo yum install -y"
     alias r="sudo yum autoremove"
     alias s="sudo yum search"
     alias u="sudo yum update && sudo yum upgrade"
 fi
 
+if hash code-insiders 2> /dev/null; then
+    alias code="code-insiders"
+#    alias c="code"
+fi
+
+if hash synaptic 2> /dev/null; then
+    alias si="sudo synaptic"
+fi
+
+if hash curljson 2> /dev/null;
+then
+    alias curl="curljson -i"
+else
+    pip install curljson
+    alias curl="curljson -i"
+fi
+
+if hash ng 2> /dev/null; then
+    alias nhs="g serve --host 0.0.0.0 --port 8080 --disable-host-check"
+    #    alias ns="ng serve --ssl --ssl-key .certs/localhost.key --ssl-cert .certs/localhost.crt"
+    alias ns="ng serve --host 0.0.0.0 --disable-host-check"
+fi
+
+alias c="ccze -A"
+
+alias docker_prune="docker system prune -a"
+alias journal_clean="journalctl --vacuum-time=10d"
+
+if hash vagrant 2> /dev/null; then
+    alias v="vagrant"
+    alias vgs="vagrant ssh"
+    alias vdu="vagrant destroy --force && vagrant up --provision"
+    alias vu="vagrant up --provision"
+    alias vd="v destroy --force"
+fi
+
+if hash exa 2> /dev/null; then
+    alias ls="exa -h --icons -l"
+    alias t='exa -T -I "node_modules|venv|.git|.angular|.idea|.yarn|.vscode" --icons'
+fi
+# endregion
+
+alias l="ls -l"
+alias ll="l"
+alias lt="ls --tree"
+alias la="ls -la"
+alias tree="lt"
+
+alias cp="cp -v"
+alias mv="mv -v"
+alias mkdir="mkdir -v"
+alias rm="rm -v"
+alias ln="ln -v"
+
+alias h="vi ~/.zsh_history"
+alias vz="vi ~/.zshrc"
+alias vb="vi ~/.bashrc"
+alias va="vi ~/.aliases.sh"
+alias vs="vi $HOME/.fn.sh $HOME/.aliases.sh $HOME/.zshrc"
+alias sz="source ~/.zshrc"
+alias vimrc="vi ~/.vimrc"
+alias di="sudo dpkg -i"
+alias ns="ng s"
+alias vn="vi ~/.npmrc"
+alias yi="yarn install"
+alias ya="yarn add"
+alias yo="yarn outdated"
+alias y="yarn"
+
+alias n="npm"
+alias ni="npm i"
+alias no="n outdated"
+
 alias q="exit"
+
+alias po="sudo poweroff"
+alias re="sudo reboot"
 
